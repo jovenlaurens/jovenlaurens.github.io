14b66bdc235c3f647aa2d8fc32702c3a8ec7cfc5
jovenlaurens <joven.nicholas@gmail.com>
2025-01-21 09:10:23 +0700

    Updated email submission form with Formspree API

--- script.js
+++ script.js
@@ -57,26 +57,3 @@ window.addEventListener('scroll', () => {
         checkAndSetMode(section2);
     }
 });
-
-// Sending email
-function sendEmail() {
-    var params = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value
-    };
-    
-    const serviceID = "service_kt2yeqe";
-    const templateID = "template_pf7ddi2";
-
-    emailjs.send(serviceID, templateID, params)
-    .then(
-        res => {
-            document.getElementById("name").value = "";
-            document.getElementById("email").value = "";
-            document.getElementById("message").value = "";
-            console.log(res);
-            alert("your message sent successfully")
-        })
-    .catch((err) => console.log(err));
-}
